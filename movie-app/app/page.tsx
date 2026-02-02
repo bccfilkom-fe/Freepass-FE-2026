67d0b40924dd9ae5ea914768017fb5ca25cd78e3
jasonenkristo123 <jasonenkristo@gmail.com>
2026-02-02 13:12:10 +0700

    installing dependencies

---
+++ movie-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-zinc-800 w-full min-h-screen text-zinc-100 font-bold flex items-center justify-center">
+      Hello World
+    </div>
+  );
+}
