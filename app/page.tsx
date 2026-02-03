792f8dd9b8fd8e3b1db1e43bf3d9596ddcf93419
arundaya <runsworkspace@gmail.com>
2026-02-03 13:12:07 +0700

    feat: added navbar and canteen grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,155 @@
-import { ComponentExample } from "@/components/component-example";
+import Navbar from "@/components/navbar";
+import { Badge } from "@/components/ui/badge";
+import {
+	Card,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+import { ShoppingBasket } from "lucide-react";
+import Image from "next/image";
+
+type TCanteen = {
+	name: string;
+	imgUrl: string;
+	rating: number;
+	status: "OPEN" | "CLOSED";
+	description: string;
+};
+
+const CanteenCard = ({
+	canteen,
+	className,
+}: {
+	canteen: TCanteen;
+	className?: string;
+}) => {
+	const StatusBadge = () => {
+		if (canteen.status === "OPEN") {
+			return <Badge variant={"default"}>Open</Badge>;
+		} else {
+			return <Badge variant={"secondary"}>Closed</Badge>;
+		}
+	};
+
+	return (
+		<Card
+			className={cn(
+				"hover:shadow-lg transition-shadow duration-300 ease-in-out",
+				className,
+			)}
+		>
+			<div className="aspect-[4/3] relative min-h-[200px]">
+				<Image
+					src={canteen.imgUrl}
+					className="object-fit"
+					alt="canteen icon"
+					style={{
+						filter: canteen.status === "CLOSED" ? "grayscale(100%)" : "none",
+					}}
+					fill
+				/>
+			</div>
+			<CardHeader>
+				<CardTitle>
+					<h3>{canteen.name}</h3>
+				</CardTitle>
+				<CardDescription>
+					<p>{canteen.description}</p>
+				</CardDescription>
+			</CardHeader>
+			<Separator />
+			<CardFooter className="gap-4">
+				<StatusBadge />
+				<p>{canteen.rating}⭐</p>
+			</CardFooter>
+		</Card>
+	);
+};
+
+const mockCanteens: TCanteen[] = [
+	{
+		name: "Canteen Foo",
+		imgUrl:
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzFmfQySJx5a258fgxg_JPzHyilvaVuHeQcA&s",
+		description: "A popular canteen known for its delicious food.",
+		rating: 4.5,
+		status: "OPEN",
+	},
+	{
+		name: "Canteen Bar",
+		imgUrl:
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzFmfQySJx5a258fgxg_JPzHyilvaVuHeQcA&s",
+		description: "Great atmosphere and tasty meals.",
+		rating: 4.2,
+		status: "CLOSED",
+	},
+	{
+		name: "Canteen Baz",
+		imgUrl:
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzFmfQySJx5a258fgxg_JPzHyilvaVuHeQcA&s",
+		description: "Affordable and quick service.",
+		rating: 4.0,
+		status: "CLOSED",
+	},
+];
+
+type TCartOverview = {
+	totalItems: number;
+};
+
+const CartButton = (props: { cart: TCartOverview; className?: string }) => {
+	const { cart, className } = props;
+	return (
+		<div
+			className={cn(
+				"rounded-full bg-primary p-2 max-w-[40%] min-w-[200px]",
+				className,
+			)}
+		>
+			<ShoppingBasket />
+		</div>
+	);
+};
 
 export default function Page() {
-return <ComponentExample />;
+	return (
+		<>
+			<div className="py-2 w-full">
+				<div className="container max-w-[90%] mx-auto flex flex-col py-4">
+					<h1 className="text-xl">
+						<span className="font-bold">Halo,</span>
+						&nbsp;Dudung!
+					</h1>
+					<p className="text-muted-foreground mt-2">
+						Mau makan apa nih hari ini?
+					</p>
+				</div>
+			</div>
+			<main className="w-screen">
+				<div>
+					<CanteenGrid className="bg-muted m-4 rounded-[2rem]" />
+				</div>
+			</main>
+			<Navbar />
+		</>
+	);
+}
+
+function CanteenGrid({ className }: { className?: string }) {
+	return (
+		<div
+			className={cn(
+				"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4",
+				className,
+			)}
+		>
+			{mockCanteens.map((canteen) => (
+				<CanteenCard key={canteen.name} canteen={canteen} />
+			))}
+		</div>
+	);
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { useHideOnScroll } from "@/hooks/use-hide-on-scroll";
+import { useOptimisticPathname } from "@/hooks/use-optimistic-pathname";
+import { cn } from "@/lib/utils";
+import {
+	ChartLine,
+	Home,
+	Landmark,
+	type LucideIcon,
+	PieChart,
+	User,
+} from "lucide-react";
+import Link, { type LinkProps } from "next/link";
+import { useRef } from "react";
+
+type NavItemProp = {
+	label: string;
+	icon: LucideIcon;
+	path: string;
+};
+
+const NavItem = ({
+	data,
+	isActive,
+	...rest
+}: {
+	data: NavItemProp;
+	isActive: boolean;
+} & Omit<LinkProps, "href">) => {
+	const Icon = data.icon;
+
+	return (
+		<Link
+			{...rest}
+			href={data.path}
+			className={cn(
+				"flex w-full flex-col items-center justify-center gap-2 rounded-md px-4 py-2 transition",
+				isActive ? "text-primary" : "text-muted-foreground hover:bg-muted",
+			)}
+		>
+			<Icon className={isActive ? "h-3 w-3" : "h-4 w-4"} />
+			{isActive && <span className="text-xs">{data.label}</span>}
+		</Link>
+	);
+};
+
+// TODO: change this to match app domain
+const navItems: Readonly<NavItemProp[]> = [
+	{ label: "Home", icon: Home, path: "/home" },
+	{ label: "Financial", icon: Landmark, path: "/financial" },
+	{ label: "Trading", icon: ChartLine, path: "/trading" },
+	{ label: "Portfolio", icon: PieChart, path: "/portfolio" },
+	{ label: "Profile", icon: User, path: "/profile" },
+];
+
+export default function Navbar() {
+	const { pathname, setOptimisticPath } = useOptimisticPathname();
+	const navRef = useRef<HTMLElement>(null);
+
+	useHideOnScroll(navRef);
+
+	return (
+		<nav
+			ref={navRef}
+			className={cn(
+				"fixed bottom-0 z-10 flex w-full gap-2 rounded-tl-2xl rounded-tr-2xl bg-muted px-2 py-4",
+				"translate-y-0 transition-transform duration-300",
+				"max-w-lg left-1/2 -translate-x-1/2",
+			)}
+		>
+			{navItems.map((item) => {
+				const isActive =
+					pathname === item.path ||
+					(item.path !== "/" && pathname.startsWith(`${item.path}/`));
+				return (
+					<NavItem
+						key={item.label}
+						data={item}
+						isActive={isActive}
+						onClick={() => setOptimisticPath(item.path)}
+					/>
+				);
+			})}
+		</nav>
+	);
+}
