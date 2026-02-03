35a78293c110a29e28fb303896418dcc886a8ef9
malika <malika.zna.27@gmail.com>
2026-02-04 04:14:43 +0700

    add actions and hooks

---
+++ src/actions/authActions.ts
@@ -0,0 +1,130 @@
+"use server"
+
+import { User } from "@supabase/supabase-js";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import z from "zod";
+import { signUpSchema } from "../lib/zodSchemas";
+import { createClient } from "../lib/supabase/server";
+
+type FormState = {
+  success: boolean,
+  message: string,
+  namaError?: string,
+  emailError?: string,
+  passError?: string,
+  email?: string,
+  nama?: string,
+  user?: User | null
+}
+
+export async function handleRegister(prevState: FormState, formData: FormData) {
+  const rawData = Object.fromEntries(formData.entries());
+  const validatedFields = signUpSchema.safeParse(rawData);
+
+  if (!validatedFields.success) {
+    const errors = z.treeifyError(validatedFields.error);
+    return {
+      success: false,
+      message: `Input tidak valid`,
+      email: rawData.email as string,
+      nama: rawData.nama as string,
+      namaError: errors.properties?.nama?.errors[0],
+      emailError: errors.properties?.email?.errors[0],
+      passError: errors.properties?.password?.errors[0],
+    };
+  }
+
+  const nama = formData.get('nama') as string;
+  const email = formData.get('email') as string;
+  const pass = formData.get('password') as string;
+
+  try {
+    const supabase = await createClient();
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: pass,
+      options: {
+        data: {
+          name: nama,
+        }
+      }
+    })
+
+    if (error) throw new Error(error.message)
+
+    return {
+      success: true,
+      message: 'Registrasi berhasil!',
+      email: email,
+      nama: nama,
+      namaError: "",
+      emailError: "",
+      passError: "",
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message: `${error}`,
+      email: email,
+      nama: nama,
+      namaError: "",
+      emailError: "",
+      passError: "",
+    }
+  }
+}
+
+export async function handleLogin(prevState: FormState, formData: FormData) {
+  const email = formData.get('email') as string;
+  const pass = formData.get('password') as string;
+
+  try {
+    const supabase = await createClient();
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: pass,
+    })
+
+    if (error) throw new Error(error.message);
+
+    const cookieStore = await cookies();
+    cookieStore.set("session_token", data.session.access_token);
+
+    // ini throw error, jadi tidak bisa begini
+    // redirect('/dashboard');
+
+    return {
+      success: true,
+      message: 'Login Berhasil!',
+      user: data.user
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message: `${error}`,
+      email: email,
+      user: null
+    }
+  }
+}
+
+export async function handleLogout(prevState: FormState) {
+  const supabase = await createClient();
+  const { error } = await supabase.auth.signOut();
+
+  if (error) return {
+    success: false,
+    message: error.message
+  }
+
+  const cookieStore = await cookies();
+  cookieStore.delete("session_token");
+
+  redirect('/login');
+
+  return {
+    success: true,
+    message: "Berhasil Logout"
+  }
+}
---
+++ src/hooks/useCategories.ts
@@ -0,0 +1,40 @@
+"use client"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createClient } from "../lib/supabase/client";
+import { category } from "../types/categories";
+
+export function useCategories() {
+  return useQuery({
+    queryKey: ["categories"],
+    queryFn: async () => {
+      const supabase = createClient();
+      const userid = (await supabase.auth.getUser()).data.user?.id;
+      const { data, error } = await supabase
+        .from("nexstore_category")
+        .select("*")
+        .eq("user_id", userid)
+        .order("id", {ascending: true})
+
+      if (error) throw new Error(error.message)
+
+      const cat: category[] = data;
+      return cat;
+    }
+  })
+}
+
+export function useDeleteCategories() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (id: number) => {
+      const supabase = createClient();
+      const response = await supabase
+        .from('nexstore_category')
+        .delete()
+        .eq('id', id)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ["categories"]})
+    }
+  })
+}
