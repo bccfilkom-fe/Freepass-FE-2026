f3032493c15bf1f5845223360dbcae8b93a1567d
arundaya <runsworkspace@gmail.com>
2026-02-03 15:14:45 +0700

    fix: correctly setup providers

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster as SonnerToaster } from "sonner";
 import { queryClient } from "@/lib/query-client";
+import { useState } from "react";
 
 export default function Providers({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+   const [qc] = useState(() => queryClient);
+
 	return (
-		<QueryClientProvider client={queryClient}>
+		<QueryClientProvider client={qc}>
 			{children}
 			<Toaster />
 		</QueryClientProvider>
