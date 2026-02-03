05d0e373e4276b05daef3b6a764449b6b1315728
arundaya <runsworkspace@gmail.com>
2026-02-04 04:58:07 +0700

    feat: added new remote-patterns in next-config

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "**",
-			},
-			{
-				protocol: "https",
-				hostname: "encrypted-tbn0.gstatic.com",
-			},
-			{
-				protocol: "https",
-				hostname: "images.unsplash.com",
-			},
-		],
-	},
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
