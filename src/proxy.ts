f71fe7a8287f42ce15f7c36fb6a48f314ef4b71a
malika <malika.zna.27@gmail.com>
2026-02-04 04:40:50 +0700

    add middleware/proxy

---
+++ src/proxy.ts
@@ -0,0 +1,22 @@
+import { cookies } from 'next/headers';
+import { NextResponse, NextRequest } from 'next/server'
+
+// hilangkan test
+const protectedRoutes = ['/categories', '/dashboard', '/movements', '/product', '/test']
+
+export async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const cookieStore = await cookies();
+  const session = cookieStore.get("session_token");
+
+  if (!session && protectedRoutes.includes(pathname)) return NextResponse.redirect(new URL('/login', request.nextUrl));
+  if (session && !protectedRoutes.includes(pathname)) return NextResponse.redirect(new URL('/dashboard', request.nextUrl));
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|.*\\.png$).*)',
+  ],
+}
